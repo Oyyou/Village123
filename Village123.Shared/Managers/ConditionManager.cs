705c913476d577e4eaf9e4880f195ca44ff71396
Niall Lewin <Lewin91@hotmail.com>
2024-05-10 15:38:00 +0100

    Added `conditionManager`

---
+++ Village123.Shared/Managers/ConditionManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Village123.Shared.Entities;
+
+namespace Village123.Shared.Managers
+{
+  public class ConditionManager
+  {
+    private Villager _villager;
+
+    public ConditionManager(Villager villager)
+    {
+      _villager = villager;
+    }
+
+    public void SetRate(string condition, float amount)
+    {
+      if (!_villager.Conditions.ContainsKey(condition))
+        return;
+
+      _villager.Conditions[condition].Rate = amount;
+    }
+
+    public void ResetCondition(string condition)
+    {
+      if (!_villager.Conditions.ContainsKey(condition))
+        return;
+
+      _villager.Conditions[condition].ResetRate();
+    }
+
+    public float GetValue(string condition)
+    {
+      if (!_villager.Conditions.ContainsKey(condition))
+        return 0f; // Not sure what to do here tbh
+
+      return _villager.Conditions[condition].Value;
+    }
+  }
+}
