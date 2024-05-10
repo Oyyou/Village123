180396ee12a52630fdfe53f6fedbebc711409e15
Niall Lewin <Lewin91@hotmail.com>
2024-05-10 11:52:07 +0100

    Simplified the action stuff

--- Village123.Shared/VillagerActions/IVillagerAction.cs
+++ Village123.Shared/VillagerActions/IVillagerAction.cs
@@ -1,7 +1,11 @@
-﻿namespace Village123.Shared.VillagerActions
+﻿using Village123.Shared.Entities;
+using Village123.Shared.Models;
+
+namespace Village123.Shared.VillagerActions
 {
   public interface IVillagerAction
   {
+    void Initialize(Villager villager, GameWorld gameWorld);
     bool Started { get; set; }
     void Start();
     void Update();
