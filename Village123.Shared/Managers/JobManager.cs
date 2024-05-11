a474442d9bec000ff385bebac2edb35f091ab477
Niall Lewin <Lewin91@hotmail.com>
2024-05-11 13:34:30 +0100

    Adding jobs

---
+++ Village123.Shared/Managers/JobManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Village123.Shared.Data;
+using Village123.Shared.Entities;
+using Village123.Shared.Models;
+
+namespace Village123.Shared.Managers
+{
+  public class JobManager
+  {
+    private readonly GameWorld _gameWorld;
+    private readonly IdData _idData;
+    private readonly JobData _jobdata;
+
+    public JobManager(
+      GameWorld gameWorld,
+      IdData idData,
+      JobData jobData
+      )
+    {
+      _gameWorld = gameWorld;
+      _idData = idData;
+      _jobdata = jobData;
+    }
+
+    public Job Add(string name, Point point)
+    {
+      var job = new Job()
+      {
+        Id = _idData.JobId++,
+        Name = name,
+        Point = point,
+      };
+
+      _jobdata.Add(job);
+
+      return job;
+    }
+  }
+}
