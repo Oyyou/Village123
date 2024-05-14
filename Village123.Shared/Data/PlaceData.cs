b3bec7c6be828cd7a89f96d3abb8a9eaaab5d944
Niall Lewin <Lewin91@hotmail.com>
2024-05-14 13:20:42 +0100

    Revert "Re-jigging the data around"
    This reverts commit 49d9dfc438145c02253b2cca881857142982de55.

--- Village123.Shared/Data/PlaceData.cs
+++ Village123.Shared/Data/PlaceData.cs
@@ -2,7 +2,6 @@
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
-using static Village123.Shared.Data.ItemTypeData;
 
 namespace Village123.Shared.Data
 {
@@ -12,7 +11,6 @@ namespace Village123.Shared.Data
 
     public class Place
     {
-      [JsonProperty("name")]
       public string Name { get; set; }
 
       [JsonProperty("type")]
@@ -28,34 +26,34 @@ namespace Village123.Shared.Data
       public Point Size { get; set; }
 
       /// <summary>
+      /// Does it block the path while under construction
+      /// </summary>
+      [JsonProperty("constructionBlocksPath")]
+      public bool ConstructionBlocksPath { get; set; }
+
+      /// <summary>
       /// Once built, does it block the path
       /// </summary>
       [JsonProperty("blocksPath")]
       public bool BlocksPath { get; set; }
 
       /// <summary>
-      /// If it's a blocking object, these points will be walkable
-      /// </summary>
-      [JsonProperty("walkablePoints")]
-      public List<Point> WalkablePoints { get; set; } = new();
-
-      /// <summary>
       /// The resources required to build
       /// </summary>
       [JsonProperty("requiredBuildResources")]
-      public Dictionary<string, int> RequiredBuildResources { get; set; } = new();
+      public Dictionary<string, int> RequiredBuildResources { get; set; }
 
       /// <summary>
       /// The skills required by the villager to be built
       /// </summary>
       [JsonProperty("requiredBuildSkills")]
-      public Dictionary<string, int> RequiredBuildSkills { get; set; } = new();
+      public Dictionary<string, int> RequiredBuildSkills { get; set; }
 
       /// <summary>
       /// The types items that can be produced
       /// </summary>
       [JsonProperty("producedItemTypes")]
-      public List<string> ProducedItemTypes { get; set; } = new();
+      public List<string> ProducedItemTypes { get; set; }
     }
 
     public static PlaceData Load()
@@ -66,13 +64,18 @@ namespace Village123.Shared.Data
       foreach (var file in placeDataFiles)
       {
         var jsonString = File.ReadAllText(file);
-        var results = JsonConvert.DeserializeObject<Dictionary<string, Place>>(jsonString);
+        var places = JsonConvert.DeserializeObject<Dictionary<string, Place>>(jsonString);
 
-        foreach (var kvp in results)
+        foreach (var kvp in places)
         {
-          if (!data.Places.ContainsKey(kvp.Key))
+          var placeName = kvp.Key;
+          var place = kvp.Value;
+
+          place.Name = placeName;
+
+          if (!data.Places.ContainsKey(placeName))
           {
-            data.Places.Add(kvp.Key, kvp.Value);
+            data.Places.Add(placeName, place);
           }
         }
       }
