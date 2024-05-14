efb37eecb646a7017ad863a57e87918361463343
Niall Lewin <Lewin91@hotmail.com>
2024-05-14 13:20:42 +0100

    Revert "Expanded the placeData"
    This reverts commit a0431746cf91fd997359fe3e29f188a5e0be1b14.

--- Village123.Shared/Data/PlaceData.cs
+++ Village123.Shared/Data/PlaceData.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
 
@@ -15,45 +14,8 @@ namespace Village123.Shared.Data
 
       [JsonProperty("type")]
       public string Type { get; set; }
-
       [JsonProperty("category")]
       public string Category { get; set; }
-
-      /// <summary>
-      /// The dimensions of the place (used for blocking paths)
-      /// </summary>
-      [JsonProperty("size")]
-      public Point Size { get; set; }
-
-      /// <summary>
-      /// Does it block the path while under construction
-      /// </summary>
-      [JsonProperty("constructionBlocksPath")]
-      public bool ConstructionBlocksPath { get; set; }
-
-      /// <summary>
-      /// Once built, does it block the path
-      /// </summary>
-      [JsonProperty("blocksPath")]
-      public bool BlocksPath { get; set; }
-
-      /// <summary>
-      /// The resources required to build
-      /// </summary>
-      [JsonProperty("requiredBuildResources")]
-      public Dictionary<string, int> RequiredBuildResources { get; set; }
-
-      /// <summary>
-      /// The skills required by the villager to be built
-      /// </summary>
-      [JsonProperty("requiredBuildSkills")]
-      public Dictionary<string, int> RequiredBuildSkills { get; set; }
-
-      /// <summary>
-      /// The types items that can be produced
-      /// </summary>
-      [JsonProperty("producedItemTypes")]
-      public List<string> ProducedItemTypes { get; set; }
     }
 
     public static PlaceData Load()
